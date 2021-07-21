bacc12f480d9a26c9c6fcae97ab80b26f96ce3f8
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-21 21:50:25 +0530

    uploaded project

---
+++ main.js
@@ -0,0 +1,35 @@
+var posX = 200;
+var posY = 200;
+var dx = 3;
+var dy = 4;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+    rect(5, posY - 40, 20, 100);
+    rect(375, mouseY, 20, 100);
+    circle(posX, posY, 20);
+    if (posX == 20) {
+        dx = -dx;
+    }
+    if (posY > mouseY && posY < mouseY + 100 && posX >= width - 20) {
+        dx = -dx;
+    }
+    if (posX >= width) {
+        stroke("#FF0000");
+        text("GAME OVER", 150, 150)
+    }
+    if (posY >= height - 20 || posY == 20) {
+        dy = -dy
+    }
+
+    for (i = 0; i < 400; i += 20) {
+        stroke("black");
+        line(200, i, 200, i + 10)
+    }
+    posX = posX + dx;
+    posY = posY + dy;
+}
